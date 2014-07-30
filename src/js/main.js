29891fb79007b52b6e712f97b989807ca8ac7ff4
Tajima Shingo <tajima-shingo@kayac.com>
2014-07-30 10:20:30 +0900

    css js mod

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 (function(global, doc, $, ns, undefined) {
-    'use strict';
+	'use strict';
+	ns = ns || {};
 
-    $(function() {
-    });
+	$(function() {
+	});
+	global.namespace = ns;
 })(this, document, jQuery, this.namespace);
