e63fc29f28afc3b6dc9bee7683a72286f4cf4860
Tajima Shingo <tajima-shingo@kayac.com>
2014-08-04 17:39:39 +0900

    fix css and add eventdispatcher

---
+++ src/js/util/EventDispatcher.js
@@ -0,0 +1,63 @@
+(function(global, doc, $, ns, undefined) {
+	'use strict';
+
+	function EventDispatcher() {
+		this._events = {};
+	}
+
+	EventDispatcher.prototype.hasEventListener = function(eventName) {
+		return !!this._events[eventName];
+	};
+
+	EventDispatcher.prototype.addEventListener = function(eventName, callback) {
+		if (this.hasEventListener(eventName)) {
+			var events = this._events[eventName];
+			for (var i in events) {
+				if (events[i] === callback) {
+					return;
+				}
+			}
+			events.push(callback);
+		}
+		else{
+			this._events[eventName] = [callback];
+		}
+		return this;
+	};
+
+	EventDispatcher.prototype.removeEventListener = function(eventName, callback) {
+		if (!this.hasEventListener(eventName)) {
+			return;
+		}
+		else{
+			var events = this._events[eventName],
+					i      = events.length,
+					index;
+			while (i--) {
+				if (events[i] === callback) {
+					index = i;
+				}
+			}
+			events.splice(index, 1);
+		}
+		return this;
+	};
+
+	EventDispatcher.prototype.fireEvent = function(eventName, opt_this) {
+		if (!this.hasEventListener(eventName)) {
+			return;
+		}
+		else{
+			var events = this._events[eventName],
+			copyEvents = $.merge([], events),
+			arg        = $.merge([], arguments);
+			arg.splice(0, 2);
+			for (var i in copyEvents) {
+				copyEvents[i].apply(opt_this || this, arg);
+			}
+		}
+	};
+
+	ns.GameStage.EventDispatcher = EventDispatcher;
+
+})(this, document, $, jQuery, this.namespace);
