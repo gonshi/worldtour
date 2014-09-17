2d1e00bc7da42b79bcb78dc01d9f55ba4cb73da7
Tajima Shingo <tajima-shingo@kayac.com>
2014-09-17 22:39:43 +0900

    increase photo num

--- src/js/view/showPhotos.js
+++ src/js/view/showPhotos.js
@@ -18,7 +18,7 @@
   ns.showPhotos = function( photos ){
     var i = 0;
     //var photosLength = photos.length;
-    var photosLength = 20;
+    var photosLength = 40;
     var maxColumnNum = 3;
     var columnNum;
     var photoMargin = $(window).width() * 0.025;
